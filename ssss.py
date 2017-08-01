a2dd2673b46cdfaed53b8ee11620c24859ec7e93
JoeTac <jtacconelli@gmail.com>
2017-08-01 14:13:43 -0400

    Add files via upload

---
+++ ssss.py
@@ -0,0 +1,69 @@
+import urllib.request
+import os.path
+import zipfile
+
+try:
+    import zlib
+    compression = zipfile.ZIP_DEFLATED
+except:
+    compression = zipfile.ZIP_STORED
+
+i = 0
+url_pattern = "http://www.sssscomic.com/comicpages/{0:d}.jpg"
+file_pattern = "{0:04d}.jpg"
+save_path = "comic"
+progress = "Downloading {0}..."
+zipping = "Writing cbz [{0}]..."
+
+chapters = {
+	"Prologue":   68,
+	"Chapter 01": 119,
+	"Chapter 02": 141,
+	"Chapter 03": 178,
+	"Chapter 04": 222,
+	"Chapter 05": 276,
+	"Chapter 06": 300,
+	"Chapter 07": 363,
+	"Chapter 08": 412,
+	"Chapter 09": 437,
+	"Chapter 10": 509,
+	"Chapter 11": 585,
+	"Chapter 12": 629,
+	"Chapter 13": 658,
+	"Chapter 14": 687,
+	"Chapter 15": 0
+}
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+for chapter in chapters.items():
+	cbz = "Stand Still. Stay Silent - " + chapter[0] + ".cbz"
+	if ( os.path.exists(cbz) ):
+		continue
+	
+	startIndex = i + 1
+	while i<chapter[1] or chapter[1]==0:
+		i = i + 1
+		url = url_pattern.format(i)
+		file = file_pattern.format(i)
+		save = save_path + "/" + file
+
+		if os.path.exists(save):
+			continue
+
+		print(progress.format(url))
+		try:
+			urllib.request.urlretrieve(url, save)
+		except:
+			print("Reached of comic pages.")
+			break
+
+	if i==chapter[1]:
+		print(zipping.format(chapter[0]))
+		zf = zipfile.ZipFile(cbz, mode='w')
+		for j in range(startIndex, chapter[1]+1):
+			file = file_pattern.format(j)
+			save = save_path + "/" + file
+			zf.write(save, compress_type=compression, arcname=file)
+		zf.close()

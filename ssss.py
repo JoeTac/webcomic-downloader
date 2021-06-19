0229cc23432138bf58712b69baa89e9c511b479d
JoeTac <1260694+JoeTac@users.noreply.github.com>
2021-06-19 01:06:39 -0400

    Update ssss.py
    added chapters

--- ssss.py
+++ ssss.py
@@ -31,7 +31,13 @@ chapters = {
 	"Chapter 12": 629,
 	"Chapter 13": 658,
 	"Chapter 14": 687,
-	"Chapter 15": 0
+	"Chapter 15": 752,
+	"Chapter 16": 782,
+	"Chapter 17": 814,
+	"Chapter 18": 863,
+	"Chapter 19": 899,
+	"Chapter 20": 924,
+	"Chapter 21": 0
 }
 
 if not os.path.exists(save_path):
